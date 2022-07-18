427aa2b055c0bfd6325958b67dc8086fe696ae6d
Your Name <lucasdosualdo@gmail.com>
2022-07-18 00:42:12 -0300

    componentização dos sotires realizada

--- src/Stories.js
+++ src/Stories.js
@@ -1,14 +1,45 @@
 import Story from "./Story"
-
-export default function Stories () {
-
+export default function Stories() {
+    const stories = [
+        {
+            image: "assets/img/9gag.svg",
+            username: "9gag"
+        },
+        {
+            image: "assets/img/meowed.svg",
+            username: "meowed"
+        },
+        {
+            image: "assets/img/barked.svg",
+            username: "barked"
+        },
+        {
+            image: "assets/img/nathanwpylestrangeplanet.svg",
+            username: "nathanwpylestrangeplanet"
+        },
+        {
+            image: "assets/img/wawawicomics.svg",
+            username: "wawawicomics"
+        },
+        {
+            image: "assets/img/respondeai.svg",
+            username: "respondeai"
+        },
+        {
+            image: "assets/img/filomoderna.svg",
+            username: "filomoderna"
+        },
+        {
+            image: "assets/img/memeriagourmet.svg",
+            username: "memeriagourmet"
+        }
+    ]
     return (
         <div class="stories">
-          <Story />
-
+            {stories.map(story => (<Story image={story.image} username={story.username} />))}
             <div class="setinha">
-              <ion-icon name="chevron-forward-circle"></ion-icon>
+                <ion-icon name="chevron-forward-circle"></ion-icon>
             </div>
-          </div>
+        </div>
     )
 }
--- src/Story.js
+++ src/Story.js
@@ -1,82 +1,12 @@
-export default function Story () {
-
+export default function Story(props) {
     return (
-        <>
         <div class="story">
-              <div class="imagem">
-                <img src="assets/img/9gag.svg" />
-              </div>
-              <div class="usuario">
-                9gag
-              </div>
+            <div class="imagem">
+                <img src={props.image} />
             </div>
-
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/meowed.svg" />
-              </div>
-              <div class="usuario">
-                meowed
-              </div>
+            <div class="usuario">
+                {props.username}
             </div>
-
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/barked.svg" />
-              </div>
-              <div class="usuario">
-                barked
-              </div>
-            </div>
-
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/nathanwpylestrangeplanet.svg" />
-              </div>
-              <div class="usuario">
-                nathanwpylestrangeplanet
-              </div>
-            </div>
-
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/wawawicomics.svg" />
-              </div>
-              <div class="usuario">
-                wawawicomics
-              </div>
-            </div>
-            
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/respondeai.svg" />
-              </div>
-              <div class="usuario">
-                respondeai
-              </div>
-            </div>
-
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/filomoderna.svg" />
-              </div>
-              <div class="usuario">
-                filomoderna
-              </div>
-            </div>
-
-            <div class="story">
-              <div class="imagem">
-                <img src="assets/img/memeriagourmet.svg" />
-              </div>
-              <div class="usuario">
-                memeriagourmet
-              </div>
-            </div>
-
-        </>
-        
-            
-         
+        </div>
     )
 }
